8bc8f2d110fb9f0425048959565958bf6a66ade0
Dylan Stankievech <dylan.stank@gmail.com>
2020-11-09 18:12:28 -0700

    Add input boxes for x / y axes And updated TODO

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
 TODO:
-* Allow .csv file upload
-* Different types of charts
+* Configurable title / x / y axes
+* Debounce input in text boxes: https://dev.to/gabe_ragland/debouncing-with-react-hooks-jci
+* Different types of charts / data file inputs? histogram? box plot? scatter plot? heatmap? line plot? geo map? graph network?
 * Maybe allow data input directly?
--- src/BarChart.js
+++ src/BarChart.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 import { applyD3 } from './d3';
 
 class BarChart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Title',
+        };
+      }
+
     componentDidMount() {
         this.handleChartUpdate();
     }
+
     componentDidUpdate(prevProps) {
         if (this.props.data && prevProps.data !== this.props.data) {
             this.handleChartUpdate();
@@ -12,7 +20,23 @@ class BarChart extends Component {
     }
 
     render() {
-        return <div id="d3_chart"></div>
+        return (
+            <div>
+                <label>
+                    Title:
+                    <input type="text" name="title" />
+                </label>
+                <label>
+                    Y-Axis:
+                    <input type="text" name="title" />
+                </label>
+                <label>
+                    X-Axis:
+                    <input type="text" name="title" />
+                </label>
+                <div id="d3_chart"></div>
+            </div>
+        );
     }
 
     handleChartUpdate() {
